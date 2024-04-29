185c56c1e8bbf42f2c2b419b92e8fbe563193a48
Max Kørner Andersen <maxkandersen@gmail.com>
2024-04-29 13:40:45 +0200

    Handin12 done!

--- miniscala/src/Week12.scala
+++ miniscala/src/Week12.scala
@@ -23,11 +23,67 @@ object Week12 {
         case Nil => z
         case Cons(h, t) => f(h, t.foldRight(z, f))
       }
+
+      def length(): Int = {
+        this match
+          case Nil => 0
+          case Cons(y, ys) => 1 + ys.length()
+      }
     }
 
     case object Nil extends List[Nothing]
 
     case class Cons[T](x: T, xs: List[T]) extends List[T]
+
+    abstract class Comparable[T] { // in real Scala code, this would be a “trait”, not an abstract class
+      /** Returns <0 if this < that, ==0 if this==that, and >0 if this>that */
+      def compareTo(that: T): Int
+    }
+    class Student(val id: Int) extends Comparable[Student] {
+      def compareTo(that: Student) = this.id - that.id
+    }
+
+
+    def merge[T <: Comparable[T]](xs: List[T], ys: List[T]): List[T] = {
+
+      (xs, ys) match {
+        case (Cons(a, as), Cons(b, bs)) =>
+          if (a.compareTo(b) <= 0) Cons(a, merge(as, ys))
+          else Cons(b, merge(xs, bs))
+        case (Cons(_, _), Nil) => xs
+        case (Nil, Cons(_, _)) => ys
+        case (Nil, Nil) => Nil
+      }
+    }
+
+    def split[T <: Comparable[T]](xs: List[T], n: Int): (List[T], List[T]) = {
+
+      assert(n >= 0, "n should be positive")
+
+      xs match {
+        case Cons(y, ys) =>
+          if (n == 0) (Nil, xs)
+          else
+            val (left, right) = split(ys, n - 1)
+            (Cons(y, left), right)
+        case Nil => throw RuntimeException("n was larger than the list")
+      }
+
+    }
+
+    def mergeSort[T <: Comparable[T]](xs: List[T]): List[T] = {
+      val n = xs.length() / 2
+      if (n == 0) xs
+      else {
+        val (left, right) = split(xs, n)
+        merge(mergeSort(left), mergeSort(right))
+      }
+    }
+
+
+    val m1: List[Student] = Cons(Student(2), Cons(Student(1), Cons(Student(5), Cons(Student(3), Cons(Student(4), Cons(Student(8), Cons(Student(6), Nil)))))))
+    println("MergeSort:")
+    println(s"${mergeSort(m1).map(s => s.id)}")
 
 
     /**
@@ -57,12 +113,19 @@ object Week12 {
           xs().foreach(f)
       }
 
-      def filter(p: T => Boolean): Stream[T] = ???
+      def filter(p: T => Boolean): Stream[T] = this match
+        case SNil => SNil
+        case SCons(x, xs) => if p(x()) then SCons(x, () => xs().filter(p)) else xs().filter(p)
 
-      def zip[U](ys: Stream[U]): Stream[(T, U)] = ???
+      def zip[U](ys: Stream[U]): Stream[(T, U)] = (this, ys) match
+        case (SCons(x, xs), SCons(y, ys)) => SCons(() => (x(), y()), () => xs().zip(ys()))
+        case _ => SNil
 
-      def take(n: Int): Stream[T] =
-        if (n == 0) SNil else SCons(() => head(), () => tail().take(n - 1))
+      def take(n: Int): Stream[T] = {
+        if (n == 0) SNil
+        else if (n == 1) SCons(() => head(), () => SNil)
+        else SCons(() => head(), () => tail().take(n - 1))
+      }
 
       def foldRight[B](z: () => B, f: (T, () => B) => B): B = this match {
         case SNil => z()
@@ -140,11 +203,16 @@ object Week12 {
     println("Cyclic:")
     cyclic.take(10).foreach(println)
 
+    println("Filter:")
+    nats.filter(x => x % 3 == 0).take(20).foreach(println)
+//    println("Filter again:")
+//    nats.filter(x => x == -1).take(10).foreach(println)
+
     println("Fibonacci:")
     fibs.take(25).foreach(println)
 
-    //    println("Nats:")
-    //    nats.filter(x => x == 3).take(1).foreach(println)
+    println("Nats:")
+    nats.filter(x => x == 3).take(1).foreach(println)
 
     def sieve(xs: Stream[Int]): Stream[Int] =
       SCons(() => xs.head(), () => sieve(xs.tail().filter(x => x % xs.head() != 0)))
@@ -159,10 +227,35 @@ object Week12 {
         SCons(() => 1, () =>
           fibs2().zip(fibs2().tail()).map(n => n._1 + n._2)))
 
+      // SCons(() => 0, SCons(() >= 1, SCons(() => 1, SCons(() => 2, ...)))).zip(SCons(() => 1, SCons(() => 1, SCons(() => 2, ...)))) = SCons(() => (0, 1), ...)
+
     println("Fibonacci again:")
     fibs2().take(25).foreach(println)
 
     println(s"Fibonacci as list: ${fibs2().take(25).toList()}")
+
+    println("Stream zip:")
+    val sa = nats.take(10)
+    val sb = fibs.take(11)
+    sa.zip(sb).foreach(println)
+
+    def unfoldRight[A, S](z: S, f: S => Option[(A, S)]): Stream[A] =
+      f(z) match {
+        case Some((h, s)) => SCons(() => h, () => unfoldRight(s, f))
+        case None => SNil
+      }
+
+    println("Ones unfolded")
+    val onesUnfolded: Stream[Int] = unfoldRight(1, (i: Int) => Some(1, 1))
+    onesUnfolded.take(10).foreach(println)
+
+    println("Nats unfolded")
+    val natsUnfolded: Stream[Int] = unfoldRight(0, (i: Int) => Some(i, i+1))
+    natsUnfolded.take(10).foreach(println)
+
+    println("Fibs unfolded")
+    val fibsUnfolded: Stream[Int] = unfoldRight((0,1), (i: Int, j: Int) => Some(i, (j, i+j)))
+    fibsUnfolded.take(10).foreach(println)
 
     class Sighting(
                     val animal: String, // Which animal
